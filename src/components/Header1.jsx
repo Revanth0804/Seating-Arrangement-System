bcf9ae53ebb499a59118c61fff167b6fca6ea121
Revanth0804 <revanth.suvvari@gmail.com>
2024-12-17 00:00:17 +0530

    login btn toggling added

--- src/components/Header1.jsx
+++ src/components/Header1.jsx
@@ -1,34 +1,56 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
+import React from 'react';
+import { NavLink, useLocation } from 'react-router-dom';
 
 function Header1() {
+  const location = useLocation(); // Get current location
+
+  // Check if the current path is the admin login page or student login page
+  const isAdminLoginPage = location.pathname === '/adminlogin';
+  const isStudentLoginPage = location.pathname === '/login';
+
   return (
     <>
-        <header1>
-        <nav class="navbar navbar-expand-lg" id="nav">
-            <img src="./src/assets/images/logo1.jpeg" alt="" id="logo"/>
-            <a class="navbar-brand" href="/" id="title">Convocation Seating Arrangement</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-              <span class="navbar-toggler-icon"></span>
-            </button>
+      <header1>
+        <nav className="navbar navbar-expand-lg" id="nav">
+          <img src="./src/assets/images/logo1.jpeg" alt="" id="logo" />
+          <a className="navbar-brand" href="/" id="title">
+            Convocation Seating Arrangement
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
 
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-              <ul class="navbar-nav mr-auto">
-                <li class="nav-item active">
-                    <NavLink to="/Login"><button id='navbtn1'>Student Login</button></NavLink>  
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav mr-auto">
+              {/* Only show the buttons that are not on the current page */}
+              {!isStudentLoginPage && (
+                <li className="nav-item active">
+                  <NavLink to="/login">
+                    <button id="navbtn1">Student Login</button>
+                  </NavLink>
                 </li>
-                <li class="nav-item active">
-                    <NavLink to="/adminlogin"><button id='navbtn1'>Admin Login</button></NavLink>  
+              )}
+              {!isAdminLoginPage && (
+                <li className="nav-item active">
+                  <NavLink to="/adminlogin">
+                    <button id="navbtn1">Admin Login</button>
+                  </NavLink>
                 </li>
-                {/* <li class="nav-item">
-                    <NavLink to="/Signup"><button id='navbtn2'>Signup</button></NavLink>
-                </li> */}
-              </ul>
-            </div>
-          </nav>
+              )}
+            </ul>
+          </div>
+        </nav>
       </header1>
     </>
-  )
+  );
 }
 
-export default Header1
+export default Header1;
